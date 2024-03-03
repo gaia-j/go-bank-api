5b448bc0a8d1a6bbc57657eb74b33294d75d62f7
Hugo Gaia <hugogaia65@gmail.com>
2024-03-03 15:32:53 -0300

    Update database schema

--- scripts/db/migrations/000001_create_all_tables.up.sql
+++ scripts/db/migrations/000001_create_all_tables.up.sql
@@ -3,16 +3,6 @@ CREATE TYPE "transfer_type" AS ENUM (
   'money'
 );
 
-CREATE TABLE "account" (
-   "id" serial PRIMARY KEY,
-   "balance" bigint NOT NULL DEFAULT 0,
-   "balance_btc" bigint NOT NULL DEFAULT 0,
-   "credit" bigint NOT NULL DEFAULT 0,
-   "users_id" serial NOT NULL,
-   "created_at" timestamp DEFAULT CURRENT_TIMESTAMP,
-   "updated_at" timestamp DEFAULT CURRENT_TIMESTAMP
-);
-
 CREATE TABLE "users" (
     "id" serial PRIMARY KEY,
     "name" varchar NOT NULL,
@@ -24,36 +14,47 @@ CREATE TABLE "users" (
     "updated_at" timestamp DEFAULT CURRENT_TIMESTAMP
 );
 
+CREATE TABLE "account" (
+    "id" serial PRIMARY KEY,
+    "account_address" varchar UNIQUE NOT NULL,
+    "balance" bigint NOT NULL DEFAULT 0,
+    "balance_btc" bigint NOT NULL DEFAULT 0,
+    "credit" bigint NOT NULL DEFAULT 0,
+    "user_id" serial NOT NULL,
+    "created_at" timestamp DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" timestamp DEFAULT CURRENT_TIMESTAMP
+);
+
 CREATE TABLE "deposit" (
-   "id" bigserial PRIMARY KEY,
-   "deposit_uuid" varchar(36) UNIQUE NOT NULL,
-   "amount" bigint NOT NULL,
-   "account_id" integer NOT NULL,
-   "created_at" timestamp DEFAULT CURRENT_TIMESTAMP
+    "id" bigserial PRIMARY KEY,
+    "deposit_uuid" varchar(36) UNIQUE NOT NULL,
+    "amount" bigint NOT NULL,
+    "account_address" varchar NOT NULL,
+    "created_at" timestamp DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE "transfer" (
     "id" bigserial PRIMARY KEY,
     "transfer_uuid" varchar(36) UNIQUE NOT NULL,
-    "origin_id" serial NOT NULL,
-    "destination_id" serial NOT NULL,
+    "origin_address" varchar NOT NULL,
+    "destination_address" varchar NOT NULL,
     "amount" bigint NOT NULL,
     "type" transfer_type NOT NULL,
     "created_at" timestamp DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE "address" (
-   "id" serial PRIMARY KEY,
-   "users_id" integer NOT NULL,
-   "street" varchar NOT NULL,
-   "neighbor" varchar NOT NULL,
-   "number" varchar NOT NULL,
-   "comp" varchar NOT NULL,
-   "cep" varchar NOT NULL,
-   "state" varchar(2) NOT NULL,
-   "city" varchar NOT NULL,
-   "created_at" timestamp DEFAULT CURRENT_TIMESTAMP,
-   "updated_at" timestamp DEFAULT CURRENT_TIMESTAMP
+    "id" serial PRIMARY KEY,
+    "user_id" integer NOT NULL,
+    "street" varchar NOT NULL,
+    "neighbor" varchar NOT NULL,
+    "number" varchar NOT NULL,
+    "comp" varchar NOT NULL,
+    "cep" varchar NOT NULL,
+    "state" varchar(2) NOT NULL,
+    "city" varchar NOT NULL,
+    "created_at" timestamp DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" timestamp DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE "card" (
@@ -69,12 +70,6 @@ CREATE TABLE "card" (
     "updated_at" timestamp DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE INDEX ON "account" ("id");
-
-CREATE INDEX ON "account" ("users_id");
-
-CREATE INDEX ON "account" ("created_at");
-
 CREATE INDEX ON "users" ("id");
 
 CREATE INDEX ON "users" ("cpf");
@@ -83,21 +78,29 @@ CREATE INDEX ON "users" ("email");
 
 CREATE INDEX ON "users" ("created_at");
 
+CREATE INDEX ON "account" ("id");
+
+CREATE INDEX ON "account" ("user_id");
+
+CREATE INDEX ON "account" ("created_at");
+
 CREATE INDEX ON "deposit" ("id");
 
 CREATE INDEX ON "deposit" ("deposit_uuid");
 
 CREATE INDEX ON "deposit" ("amount");
 
+CREATE INDEX ON "deposit" ("account_address");
+
 CREATE INDEX ON "deposit" ("created_at");
 
 CREATE INDEX ON "transfer" ("id");
 
-CREATE INDEX ON "transfer" ("origin_id");
+CREATE INDEX ON "transfer" ("origin_address");
 
-CREATE INDEX ON "transfer" ("destination_id");
+CREATE INDEX ON "transfer" ("destination_address");
 
-CREATE INDEX ON "transfer" ("destination_id", "origin_id");
+CREATE INDEX ON "transfer" ("destination_address", "origin_address");
 
 CREATE INDEX ON "transfer" ("type");
 
@@ -107,7 +110,7 @@ CREATE INDEX ON "transfer" ("created_at");
 
 CREATE INDEX ON "address" ("id");
 
-CREATE INDEX ON "address" ("users_id");
+CREATE INDEX ON "address" ("user_id");
 
 CREATE INDEX ON "address" ("neighbor");
 
@@ -123,14 +126,14 @@ CREATE INDEX ON "card" ("id");
 
 CREATE INDEX ON "card" ("account_id");
 
-ALTER TABLE "account" ADD FOREIGN KEY ("users_id") REFERENCES "users" ("id");
+ALTER TABLE "account" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
 
-ALTER TABLE "deposit" ADD FOREIGN KEY ("account_id") REFERENCES "account" ("id");
+ALTER TABLE "deposit" ADD FOREIGN KEY ("account_address") REFERENCES "account" ("account_address");
 
-ALTER TABLE "transfer" ADD FOREIGN KEY ("origin_id") REFERENCES "users" ("id");
+ALTER TABLE "transfer" ADD FOREIGN KEY ("origin_address") REFERENCES "account" ("account_address");
 
-ALTER TABLE "transfer" ADD FOREIGN KEY ("destination_id") REFERENCES "users" ("id");
+ALTER TABLE "transfer" ADD FOREIGN KEY ("destination_address") REFERENCES "account" ("account_address");
 
-ALTER TABLE "address" ADD FOREIGN KEY ("users_id") REFERENCES "users" ("id");
+ALTER TABLE "address" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
 
 ALTER TABLE "card" ADD FOREIGN KEY ("account_id") REFERENCES "account" ("id");
